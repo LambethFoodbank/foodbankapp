bdf2b4f0facc8d0a16e016ac47599d1e6011eb92
Lincoln Stone <lincoln.stone@softwire.com>
2025-06-19 00:24:37 +0100

    #688: Update data seeding for new version of snaplet (#696)

--- supabase/seed/dateData.ts
+++ supabase/seed/dateData.ts
@@ -1,9 +1,9 @@
 import seedrandom from "seedrandom";
 
-export const earliestParcelOrEventDate = new Date(2024, 0, 1); // 2024/01/01
-export const latestEventDate = new Date(2024, 4, 1); // 2024/05/01
-export const farFutureDate = new Date(2025, 0, 1); // 2025/01/01
-export const parcelCreationDateTime = new Date("2023-12-31T12:00:00");
+export const earliestParcelOrEventDate = new Date(2025, 5, 1); // 2025-06-01
+export const latestEventDate = new Date(2025, 11, 1); // 2025-12-01
+export const farFutureDate = new Date(2026, 5, 1); // 2026-06-01
+export const parcelCreationDateTime = new Date("2024-12-31T12:00:00");
 
 export function getPseudoRandomDateBetween(start: Date, end: Date, seed: string): Date {
     const randomNumberGenerator = seedrandom(seed);
