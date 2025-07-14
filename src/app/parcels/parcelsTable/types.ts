c3033084f4211ea41c9bb54492f072596f332383
mihaicutaiulia <Iulia.Mihaicuta@softwire.com>
2025-07-14 15:23:03 +0300

    added list_type column to parcels_plus

--- src/app/parcels/parcelsTable/types.ts
+++ src/app/parcels/parcelsTable/types.ts
@@ -15,6 +15,7 @@ export interface ParcelsTableRow {
         collectionCentreName: string;
         collectionCentreAcronym: string;
         congestionChargeApplies: boolean;
+        hotelDelivery: string;
     };
     packingSlot: string | null;
     collectionDatetime: Date | null;
