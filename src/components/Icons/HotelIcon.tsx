fe18a98afaf3ef407c4f4fca43473fb89e509464
mihaicutaiulia <Iulia.Mihaicuta@softwire.com>
2025-07-14 15:30:14 +0300

    VFB-389: added eof

--- src/components/Icons/HotelIcon.tsx
+++ src/components/Icons/HotelIcon.tsx
@@ -3,11 +3,11 @@ import Icon from "@/components/Icons/Icon";
 import { faHotel } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
-  color?: string;
+    color?: string;
 }
 
 const HotelIcon: React.FC<Props> = (props) => {
-  return <Icon icon={faHotel} onHoverText="Deliver to hotel" color={props.color} />;
+    return <Icon icon={faHotel} onHoverText="Deliver to hotel" color={props.color} />;
 };
 
 export default HotelIcon;
