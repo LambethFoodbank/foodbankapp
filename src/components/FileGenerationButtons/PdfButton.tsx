6f53581c2cc35842817647dd1e003a4ea38b84f3
mihaicutaiulia <Iulia.Mihaicuta@softwire.com>
2025-07-16 16:15:36 +0300

    VFB-386: changed formatName back to the initial value

--- src/components/FileGenerationButtons/PdfButton.tsx
+++ src/components/FileGenerationButtons/PdfButton.tsx
@@ -21,7 +21,7 @@ const PdfButton = <Data, ErrorType extends string>({
     pdfComponent: PdfComponent,
     onFileCreationCompleted: onFileCreationCompleted = () => undefined,
     onFileCreationFailed: onFileCreationFailed,
-    formatName = false,
+    formatName = true,
     disabled = false,
     focusOnButton = false,
     formSubmitButton = false,
