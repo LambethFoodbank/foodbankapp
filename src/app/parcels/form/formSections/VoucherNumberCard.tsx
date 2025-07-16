30901ee93120c526cda5a2324ddebc1171edcb89
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-16 17:24:15 +0300

    VFB-396: add email and phone number input validation

--- src/app/parcels/form/formSections/VoucherNumberCard.tsx
+++ src/app/parcels/form/formSections/VoucherNumberCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FreeFormTextInput from "@/components/DataInput/FreeFormTextInput";
-import { errorExists, getErrorText, onChangeText } from "@/components/Form/formFunctions";
+import { errorExists, Errors, getErrorText, onChangeText } from "@/components/Form/formFunctions";
 import GenericFormCard from "@/components/Form/GenericFormCard";
 import { ErrorText, GappedDiv } from "@/components/Form/formStyling";
 import { ParcelCardProps } from "../ParcelForm";
@@ -28,39 +28,49 @@ const VoucherNumberCard: React.FC<ParcelCardProps> = ({
                     onChange={onChangeText(fieldSetter, errorSetter, "voucherNumber", {
                         required: voucherNumberIsRequired,
                     })}
-                    value={fields.voucherNumber ?? undefined}
+                    value={fields.voucherNumber ?? ""}
                 />
-                <ErrorText>{getErrorText(formErrors.voucherNumber)}</ErrorText>
                 <FreeFormTextInput
                     label="Referral Agency"
                     onChange={onChangeText(fieldSetter, errorSetter, "referralAgency", {
                         required: false,
                     })}
-                    value={fields.referralAgency ?? undefined}
+                    value={fields.referralAgency ?? ""}
                 />
                 <FreeFormTextInput
                     label="Referrer Name"
                     onChange={onChangeText(fieldSetter, errorSetter, "referrerName", {
                         required: false,
                     })}
-                    value={fields.referrerName ?? undefined}
+                    value={fields.referrerName ?? ""}
                 />
                 <FreeFormTextInput
                     label="Referrer Email"
+                    error={errorExists(formErrors.referrerEmail)}
+                    helperText={
+                        formErrors.referrerEmail
+                            ? getErrorText(formErrors.referrerEmail)
+                            : undefined
+                    }
                     onChange={onChangeText(fieldSetter, errorSetter, "referrerEmail", {
                         required: false,
                         regex: emailRegex,
                     })}
-                    value={fields.referrerEmail ?? undefined}
+                    value={fields.referrerEmail ?? ""}
                 />
                 <FreeFormTextInput
                     label="Referrer Phone"
+                    error={errorExists(formErrors.referrerPhone)}
+                    helperText={
+                        formErrors.referrerPhone
+                            ? getErrorText(formErrors.referrerPhone)
+                            : undefined
+                    }
                     onChange={onChangeText(fieldSetter, errorSetter, "referrerPhone", {
                         required: false,
                         regex: phoneNumberRegex,
-                        formattingFunction: formatPhoneNumber,
                     })}
-                    value={fields.referrerPhone ?? undefined}
+                    value={fields.referrerPhone ?? ""}
                 />
             </GappedDiv>
         </GenericFormCard>
