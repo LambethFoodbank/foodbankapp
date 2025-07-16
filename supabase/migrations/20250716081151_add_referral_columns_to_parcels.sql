8908b0c672c409f081f3826b24e2bf053a18ce09
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-16 11:13:38 +0300

    VFB-396: added a new migration with new columns in parcels table

---
+++ supabase/migrations/20250716081151_add_referral_columns_to_parcels.sql
@@ -0,0 +1,9 @@
+alter table "public"."parcels" add column "referral_agency" text;
+
+alter table "public"."parcels" add column "referrer_email" text;
+
+alter table "public"."parcels" add column "referrer_name" text;
+
+alter table "public"."parcels" add column "referrer_phone" text;
+
+
