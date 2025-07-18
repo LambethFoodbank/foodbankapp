66f1291a6b7415ce504fdfa0368e26171a310342
Stefan Ghenescu <stefan.ghenescu@softwire.com>
2025-07-18 17:11:27 +0300

    VFB-406: add migration for dietary requirements plus table

---
+++ supabase/migrations/20250718124833_add_dietary_requirements_plus_view_table.sql
@@ -0,0 +1,16 @@
+create or replace view "public"."dietary_requirements_plus" as  SELECT d.id,
+    l.item_name,
+    d.halal,
+    d.vegetarian,
+    d.vegan,
+    d.meat,
+    d.gluten_free,
+    d.pescatarian,
+    d.dairy_free,
+    d.seafood_allergy,
+    d.pet_food
+   FROM (dietary_requirements d
+     JOIN lists l ON ((d.id = l.primary_key)));
+
+
+
