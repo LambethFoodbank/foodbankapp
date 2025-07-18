5f3ffba24ce369f76bf711d0ef6f7917aacd2522
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-18 17:11:24 +0300

    VFB-411: updated supabase function, added policy, realtime on

--- src/databaseTypesFile.ts
+++ src/databaseTypesFile.ts
@@ -1087,6 +1087,13 @@ export type Database = {
         }
         Returns: undefined
       }
+      delivery_area_order_swap: {
+        Args: {
+          id1: string
+          id2: string
+        }
+        Returns: undefined
+      }
       insert_client_and_family: {
         Args: {
           clientrecord: Json
