f056a3ce03f0f4038f734536b9f5139763ddfa52
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-18 17:40:16 +0300

    VFB-403: added Modal for toggling available days

---
+++ src/app/admin/collectionCentresTable/CollectionCentreAvailableDaysModal.tsx
@@ -0,0 +1,222 @@
+"use client";
+
+import { faShoePrints } from "@fortawesome/free-solid-svg-icons";
+import { FormGroup } from "@mui/material";
+import Button from "@mui/material/Button";
+import React, { useEffect, useState } from "react";
+import styled, { useTheme } from "styled-components";
+import {
+    CollectionCentresTableRow,
+    FormattedAvailableDays,
+    FormattedAvailableDaysWithPrimaryKey,
+    updateDbCollectionCentreAvailableDays,
+} from "@/app/admin/collectionCentresTable/CollectionCentreActions";
+import { ErrorSecondaryText } from "@/app/errorStylingandMessages";
+import { Heading } from "@/app/parcels/ActionBar/ActionModals/GeneralActionModal";
+import { DAYSOFWEEK_ARRAY } from "@/common/databaseDaysOfWeek";
+import CheckboxInput from "@/components/DataInput/CheckboxInput";
+import Icon from "@/components/Icons/Icon";
+import Modal from "@/components/Modal/Modal";
+import {
+    ButtonsDiv,
+    Centerer,
+    ContentDiv,
+    OutsideDiv,
+    SpaceBetween,
+} from "@/components/Modal/ModalFormStyles";
+import { AuditLog, sendAuditLog } from "@/server/auditLog";
+
+interface Props {
+    selectedCollectionCentreInfo: CollectionCentresTableRow | null;
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+const ModalAvailableDaysContainer = styled.div`
+    max-height: 40vh;
+    overflow-y: auto;
+`;
+
+const ModalAvailableDaysRow = styled.div`
+    width: 20rem;
+`;
+
+function getBaseAuditLogForCollectionCentreAvailableDays(
+    action: string,
+    availableDaysWithPrimaryKey: FormattedAvailableDaysWithPrimaryKey
+): Pick<AuditLog, "action" | "content" | "collectionCentreId"> {
+    const availableDays = Object.fromEntries(
+        availableDaysWithPrimaryKey.availableDays.map((availaleDays) => [
+            availaleDays.day,
+            availaleDays.isActive,
+        ])
+    );
+    return {
+        action,
+        content: {
+            availableDays,
+        },
+        collectionCentreId: availableDaysWithPrimaryKey.primaryKey,
+    };
+}
+
+const formatCollectionCentreAvailableDaysDbData = (
+    row: CollectionCentresTableRow
+): FormattedAvailableDaysWithPrimaryKey => {
+    let formattedAvailableDays: FormattedAvailableDays[];
+
+    if (row.availableDays === null || row.availableDays === undefined) {
+        formattedAvailableDays = [];
+    } else {
+        formattedAvailableDays = row.availableDays.map((availableDays) => {
+            return {
+                day:
+                    availableDays.day !== null
+                        ? availableDays.day
+                        : DAYSOFWEEK_ARRAY[DAYSOFWEEK_ARRAY.length - 1],
+                isActive: availableDays.is_active !== null ? availableDays.is_active : false,
+            };
+        });
+    }
+
+    return {
+        primaryKey: row.id,
+        availableDays: formattedAvailableDays,
+    };
+};
+
+const CollectionCentreAvailableDaysModal: React.FC<Props> = (props) => {
+    const [availableDaysModalData, setAvailableDaysModalData] =
+        useState<FormattedAvailableDaysWithPrimaryKey | null>(null);
+    const [availableDaysModalErrorMessage, setAvailableDaysModalErrorMessage] = useState<
+        string | null
+    >(null);
+
+    const theme = useTheme();
+
+    useEffect(() => {
+        if (props.selectedCollectionCentreInfo) {
+            setAvailableDaysModalData(
+                formatCollectionCentreAvailableDaysDbData(props.selectedCollectionCentreInfo)
+            );
+        }
+    }, [props.selectedCollectionCentreInfo]);
+
+    const handleModalSaveClick = async (): Promise<void> => {
+        if (availableDaysModalData === null) {
+            return;
+        }
+        const { error: updateAvailableDaysError } =
+            await updateDbCollectionCentreAvailableDays(availableDaysModalData);
+        const baseAuditLog = getBaseAuditLogForCollectionCentreAvailableDays(
+            "update collection centre available days",
+            availableDaysModalData
+        );
+
+        if (updateAvailableDaysError) {
+            setAvailableDaysModalErrorMessage(
+                `Failed to update the collection centre available days. Log ID: ${updateAvailableDaysError.logId}`
+            );
+            await sendAuditLog({
+                ...baseAuditLog,
+                wasSuccess: false,
+                logId: updateAvailableDaysError.logId,
+            });
+        }
+
+        await sendAuditLog({ ...baseAuditLog, wasSuccess: true });
+        props.onClose();
+    };
+
+    const toggleAvailableDaysInModalData = (dayLabel: string): void => {
+        if (!availableDaysModalData) {
+            return;
+        }
+
+        const availableDaysIndex = availableDaysModalData.availableDays.findIndex(
+            (availableday) => availableday.day === dayLabel
+        );
+        const availableDay = availableDaysModalData.availableDays[availableDaysIndex];
+        if (!availableDay) {
+            return;
+        }
+
+        availableDay.isActive = !availableDay.isActive;
+
+        const updatedAvailableDaysData: FormattedAvailableDaysWithPrimaryKey = {
+            ...availableDaysModalData,
+            availableDays: availableDaysModalData.availableDays,
+        };
+
+        setAvailableDaysModalData(updatedAvailableDaysData);
+    };
+
+    return (
+        <Modal
+            header={
+                <>
+                    <Icon icon={faShoePrints} color={theme.primary.largeForeground[2]} />
+                    Edit Collection Available Days
+                </>
+            }
+            isOpen={props.isOpen}
+            onClose={() => {
+                props.onClose();
+            }}
+            headerId="expandedCollectionCentreAvailableDaysModal"
+            testId="CollectionCentreAvailableDaysModal"
+            maxWidth="xs"
+            footer={
+                <SpaceBetween>
+                    <Button
+                        onClick={handleModalSaveClick}
+                        variant="contained"
+                        data-testid="SaveDaysCloseModal"
+                    >
+                        Save
+                    </Button>
+                </SpaceBetween>
+            }
+        >
+            <OutsideDiv>
+                <ContentDiv>
+                    <Centerer>
+                        <Heading>{props.selectedCollectionCentreInfo?.name}</Heading>
+                    </Centerer>
+                    <Centerer>
+                        <ModalAvailableDaysContainer aria-label="List of defined available days">
+                            <FormGroup>
+                                {availableDaysModalData &&
+                                    availableDaysModalData.availableDays.map((availableDay) => {
+                                        return (
+                                            <ModalAvailableDaysRow key={availableDay.day}>
+                                                <SpaceBetween>
+                                                    <CheckboxInput
+                                                        label={availableDay.day}
+                                                        checked={availableDay.isActive}
+                                                        onChange={() =>
+                                                            toggleAvailableDaysInModalData(
+                                                                availableDay.day
+                                                            )
+                                                        }
+                                                        ariaLabel="Available Day"
+                                                    />
+                                                </SpaceBetween>
+                                            </ModalAvailableDaysRow>
+                                        );
+                                    })}
+                            </FormGroup>
+                        </ModalAvailableDaysContainer>
+                    </Centerer>
+                </ContentDiv>
+                <ButtonsDiv>
+                    {availableDaysModalErrorMessage && (
+                        <ErrorSecondaryText>{availableDaysModalErrorMessage}</ErrorSecondaryText>
+                    )}
+                </ButtonsDiv>
+            </OutsideDiv>
+        </Modal>
+    );
+};
+
+export default CollectionCentreAvailableDaysModal;
