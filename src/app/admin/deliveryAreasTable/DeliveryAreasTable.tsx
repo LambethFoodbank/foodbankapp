f14c3a50ed1a1123f24598b6719ad8c63e0aa54b
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-21 08:56:33 +0300

    VFB-411: fixed regex so now only valid postcodes can be input

--- src/app/admin/deliveryAreasTable/DeliveryAreasTable.tsx
+++ src/app/admin/deliveryAreasTable/DeliveryAreasTable.tsx
@@ -49,19 +49,26 @@ export interface DeliveryAreasRow {
 }
 
 const formatPostcode = (value: string): string => {
+    value = value.toUpperCase();
     value = value.slice(0, 4);
     const regex = /^(GIR|[A-Z]{1,2}[0-9][0-9A-Z]?)$/;
-    value = value.slice(0, 4);
+    console.log("before:", value);
     if (isNaN(Number(value.charAt(3)))) {
         value = value.slice(0,3);
-    }
+    } 
+    console.log("2:", value);
     if (isNaN(Number(value.charAt(2)))) {
         value = value.slice(0,2);
     } 
-    if (regex.exec(value)?.includes(value, 1)) {
+    console.log("3:", value);
+    if (!value.charAt(1).match(/[a-zA-Z]/)) {
         value = value.slice(0, 1);
     }
-    return value.toUpperCase();
+    if (!value.charAt(0).match(/[a-zA-Z]/)) {
+        value = value.slice(0, 0);
+    }
+    console.log("after:", value);
+    return value;
 };
 
 function EditToolbar(props: EditToolbarProps): React.JSX.Element {
