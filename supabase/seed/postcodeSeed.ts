ea8202ea3c01a085d48505e456900d8265d5bba9
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-21 14:48:30 +0300

    VFB-411: added seeds for delivery_areas table and set realtime on

---
+++ supabase/seed/postcodeSeed.ts
@@ -0,0 +1,167 @@
+export const possibleDeliveryAreas = [
+    {
+        postcode: "E1",
+        isDeliverable: true,
+        order: 1,
+    },
+    {
+        postcode: "E5",
+        isDeliverable: true,
+        order: 2,
+    },
+    {
+        postcode: "E13",
+        isDeliverable: true,
+        order: 3,
+    },
+    {
+        postcode: "N13",
+        isDeliverable: true,
+        order: 4,
+    },
+    {
+        postcode: "N15",
+        isDeliverable: true,
+        order: 5,
+    },
+    {
+        postcode: "NW6",
+        isDeliverable: true,
+        order: 6,
+    },
+    {
+        postcode: "SE7",
+        isDeliverable: true,
+        order: 7,
+    },
+    {
+        postcode: "SE8",
+        isDeliverable: true,
+        order: 8,
+    },
+    {
+        postcode: "SE11",
+        isDeliverable: true,
+        order: 9,
+    },
+    {
+        postcode: "SE12",
+        isDeliverable: true,
+        order: 10,
+    },
+    {
+        postcode: "SE15",
+        isDeliverable: true,
+        order: 11,
+    },
+    {
+        postcode: "SE21",
+        isDeliverable: true,
+        order: 12,
+    },
+    {
+        postcode: "SE24",
+        isDeliverable: true,
+        order: 13,
+    },
+    {
+        postcode: "SW1A",
+        isDeliverable: true,
+        order: 14,
+    },
+    {
+        postcode: "SW1H",
+        isDeliverable: true,
+        order: 15,
+    },
+    {
+        postcode: "SW1P",
+        isDeliverable: true,
+        order: 16,
+    },
+    {
+        postcode: "SW4",
+        isDeliverable: true,
+        order: 17,
+    },
+    {
+        postcode: "SW7",
+        isDeliverable: true,
+        order: 18,
+    },
+    {
+        postcode: "AB2",
+        isDeliverable: true,
+        order: 19,
+    },
+    {
+        postcode: "SW8",
+        isDeliverable: true,
+        order: 20,
+    },
+    {
+        postcode: "SW10",
+        isDeliverable: true,
+        order: 21,
+    },
+    {
+        postcode: "SW14",
+        isDeliverable: true,
+        order: 22,
+    },
+    {
+        postcode: "SW15",
+        isDeliverable: true,
+        order: 23,
+    },
+    {
+        postcode: "SW16",
+        isDeliverable: true,
+        order: 24,
+    },
+    {
+        postcode: "SW1W",
+        isDeliverable: true,
+        order: 25,
+    },
+    {
+        postcode: "W1B",
+        isDeliverable: true,
+        order: 26,
+    },
+    {
+        postcode: "W1C",
+        isDeliverable: true,
+        order: 27,
+    },
+    {
+        postcode: "W1F",
+        isDeliverable: true,
+        order: 28,
+    },
+    {
+        postcode: "W1J",
+        isDeliverable: true,
+        order: 29,
+    },
+    {
+        postcode: "WC1H",
+        isDeliverable: true,
+        order: 30,
+    },
+    {
+        postcode: "WC1X",
+        isDeliverable: true,
+        order: 31,
+    },
+    {
+        postcode: "WC2H",
+        isDeliverable: true,
+        order: 32,
+    },
+    {
+        postcode: "WC2R",
+        isDeliverable: true,
+        order: 33,
+    },
+];
