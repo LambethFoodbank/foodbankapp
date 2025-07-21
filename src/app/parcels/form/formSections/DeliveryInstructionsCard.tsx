4ab245fbacf7036c6b528f071ca7112d527056e0
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-21 11:16:05 +0300

    VFB-419: added parcel-specific card

---
+++ src/app/parcels/form/formSections/DeliveryInstructionsCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ParcelCardProps } from "@/app/parcels/form/ParcelForm";
+import FreeFormTextInput from "@/components/DataInput/FreeFormTextInput";
+import {
+    errorExists,
+    getDefaultTextValue,
+    getErrorText,
+    onChangeText,
+} from "@/components/Form/formFunctions";
+import GenericFormCard from "@/components/Form/GenericFormCard";
+
+export const MAX_CHARACTERS = 380;
+
+const DeliveryInstructionsCard: React.FC<ParcelCardProps> = ({
+    formErrors,
+    errorSetter,
+    fieldSetter,
+    fields,
+}) => {
+    return (
+        <GenericFormCard
+            title="Delivery Instructions"
+            required={false}
+            text="For example: The doorbell does not work, use the door code: 123456."
+        >
+            <FreeFormTextInput
+                label="Delivery Instructions"
+                defaultValue={getDefaultTextValue(fields, "deliveryInstructions")}
+                error={errorExists(formErrors.deliveryInstructions)}
+                helperText={getErrorText(formErrors.deliveryInstructions, MAX_CHARACTERS)}
+                onChange={onChangeText(fieldSetter, errorSetter, "deliveryInstructions", {
+                    maxCharacters: MAX_CHARACTERS,
+                })}
+                multiline
+            />
+        </GenericFormCard>
+    );
+};
+
+export default DeliveryInstructionsCard;
