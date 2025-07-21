544dd24dc3166dde8e5cfa958ad440ab2aeb5c14
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-21 18:03:55 +0300

    VFB-412: modified parcel plus to contain delivery area and added the filter by delivery area buttonm to parcels page

--- src/databaseTypesFile.ts
+++ src/databaseTypesFile.ts
@@ -906,6 +906,7 @@ export type Database = {
           collection_datetime: string | null
           created_at: string | null
           family_count: number | null
+          is_deliverable: boolean | null
           is_delivery: boolean | null
           last_status_event_data: string | null
           last_status_event_name: string | null
