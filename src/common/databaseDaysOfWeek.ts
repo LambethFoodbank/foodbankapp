4a9a518986188fdf1076fb1f162ce22f5914a684
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-21 16:50:35 +0300

    VFB-404: made sunday the first day of the week

--- src/common/databaseDaysOfWeek.ts
+++ src/common/databaseDaysOfWeek.ts
@@ -3,13 +3,13 @@ import { Database } from "@/databaseTypesFile";
 export type DaysOfWeekType = Database["public"]["Enums"]["day_of_week"] | "";
 
 export const DAYSOFWEEK_ARRAY: DaysOfWeekType[] = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
     "",
 ] as const;
 
