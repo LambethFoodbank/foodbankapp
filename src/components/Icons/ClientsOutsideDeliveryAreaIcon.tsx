c446600ae50045a944fddca92cca7e7c4b8265b0
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-23 18:06:18 +0300

    VFB-414: changed the icon color to a darker yellow

--- src/components/Icons/ClientsOutsideDeliveryAreaIcon.tsx
+++ src/components/Icons/ClientsOutsideDeliveryAreaIcon.tsx
@@ -3,7 +3,7 @@ import Icon from "@/components/Icons/Icon";
 import { faWarning } from "@fortawesome/free-solid-svg-icons";
 
 const ClientOutsideDeliveryAreaIcon: React.FC = () => {
-    return <Icon icon={faWarning} onHoverText="Outside Delivery Area" color="yellow" />;
+    return <Icon icon={faWarning} onHoverText="Outside Delivery Area" color="#B8860B" />;
 };
 
 export default ClientOutsideDeliveryAreaIcon;
