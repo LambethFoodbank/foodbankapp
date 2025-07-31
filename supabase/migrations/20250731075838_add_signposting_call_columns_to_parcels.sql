cf8541706b8411bece90bcf5dabf36d449580194
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-31 13:04:49 +0300

    VFB-443: added migrations for adding/populating signposting columns

---
+++ supabase/migrations/20250731075838_add_signposting_call_columns_to_parcels.sql
@@ -0,0 +1,5 @@
+alter table "public"."parcels" add column "signposting_call_reasons" text[] default '{}'::text[];
+
+alter table "public"."parcels" add column "signposting_call_required" boolean default false;
+
+
