d9e1d592a20c06aad8711da6f7b974b35612128f
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-31 13:09:17 +0300

    VFB-443: added signposting fields to parcel seed

--- supabase/seed/seed.ts
+++ supabase/seed/seed.ts
@@ -174,7 +174,13 @@ const main = async (): Promise<never> => {
                         ),
                     list_type: () => copycat.oneOf(ctx.seed, possibleListTypesWeighted),
                     flagged_for_attention: (ctx) => copycat.bool(ctx.seed),
-
+                    signposting_call_required: (ctx) => copycat.bool(ctx.seed),
+                    signposting_call_reasons: (ctx) =>
+                        copycat.someOf(
+                            ctx.seed,
+                            [0, possibleSignpostingCallReasons.length],
+                            possibleSignpostingCallReasons
+                        ),
                     referral_agency: () => agency,
                     referrer_name: () => (agency ? copycat.fullName(ctx.seed) : ""),
                     referrer_email: () => (agency ? copycat.email(ctx.seed) : ""),
