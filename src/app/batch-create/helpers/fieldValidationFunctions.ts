182eaecc24f27fa9f63173655fcdc92ce66447eb
ZOO\AleMar <alexandra.mardale@softwire.com>
2025-08-01 19:46:17 +0300

    VFB-467: Remove all batch-create code and tests

--- src/app/batch-create/helpers/fieldValidationFunctions.ts
+++
@@ -1,7 +0,0 @@
-import { GridPreProcessEditCellProps } from "@mui/x-data-grid";
-import { phoneNumberRegex, phoneNumberFormatSymbolsRegex } from "@/common/format";
-
-export const isPhoneNumberValid = (params: GridPreProcessEditCellProps): boolean => {
-    const unformattedInput = params.props.value.replaceAll(phoneNumberFormatSymbolsRegex, "");
-    return unformattedInput.match(phoneNumberRegex) === null;
-};
