3a26c296f68cb0f34bb3efed32cd45bb7db742df
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-08-11 17:59:56 +0300

    VFB-404: removed unnecessary daysOfWeek array

--- src/common/databaseDaysOfWeek.ts
+++ src/common/databaseDaysOfWeek.ts
@@ -1,14 +1,3 @@
 import { Database } from "@/databaseTypesFile";
 
 export type DaysOfWeekType = Database["public"]["Enums"]["day_of_week"] | "";
-
-export const DAYSOFWEEK_ARRAY: DaysOfWeekType[] = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "",
-] as const;
