d67b6b660a0a29bb91d9b1d3f3d6a411eda4cfa4
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-08-28 16:32:25 +0300

    VFB-409: added parcel column styling properties

--- src/app/parcels/parcelsTable/styles.ts
+++ src/app/parcels/parcelsTable/styles.ts
@@ -7,34 +7,61 @@ export const parcelTableColumnStyleOptions = {
     fullName: {
         grow: 1.2,
         minWidth: "6.5rem",
+        maxWidth: "15rem",
     },
     familyCategory: {
         hide: 550,
         grow: 0.7,
         minWidth: "6rem",
+        maxWidth: "10rem",
     },
     addressPostcode: {
         hide: 800,
         grow: 0.6,
         minWidth: "6rem",
+        maxWidth: "10rem",
+    },
+    phoneNumber: {
+        maxWidth: "10rem",
+        minWidth: "6rem",
+        grow: 0.6,
+    },
+    email: {
+        hide: 550,
+        grow: 0.7,
+        minWidth: "8rem",
+        maxWidth: "20rem",
+    },
+    voucherNumber: {
+        maxWidth: "10rem",
+        minWidth: "6rem",
+        grow: 0.5,
     },
     deliveryCollection: {
         grow: 0.6,
-        minWidth: "6rem",
+        maxWidth: "6rem",
+        minWidth: "5rem",
     },
     packingDate: {
         hide: 800,
         grow: 0.7,
-        minWidth: "7rem",
+        maxWidth: "7rem",
+        minWidth: "5rem",
     },
     packingSlot: {
         hide: 800,
         grow: 0.5,
         minWidth: "4rem",
+        maxWidth: "4rem",
     },
     lastStatus: {
-        grow: 3,
+        grow: 2,
         maxWidth: "30rem",
         minWidth: "10rem",
+    },
+    createdAt: {
+        grow: 0.7,
+        maxWidth: "7rem",
+        minWidth: "5rem",
     },
 };
