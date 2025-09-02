aa507860eecb737bc5e7b005fc7177f0859e72d4
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-09-02 11:59:52 +0300

    VFB-404: set default value for available days column

--- supabase/migrations/20250901124009_add_column_available_days_to_collection_centre_table.sql
+++ supabase/migrations/20250901124009_add_column_available_days_to_collection_centre_table.sql
@@ -4,3 +4,13 @@ create type "public"."collection_availability_day" as ("day" day_of_week, "is_ac
 
 alter table "public"."collection_centres" add column "available_days" collection_availability_day[];
 alter table "public"."collection_centres" alter column "available_days" set not null;
+alter table "public"."collection_centres" alter column "available_days" set default
+    ARRAY[
+        ('Monday', false),
+        ('Tuesday', false),
+        ('Wednesday', false),
+        ('Thursday', false),
+        ('Friday', false),
+        ('Saturday', false),
+        ('Sunday', false)
+    ]::collection_availability_day[];
