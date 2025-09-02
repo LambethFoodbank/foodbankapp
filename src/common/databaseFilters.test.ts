244fed55aef467ee617a6d7c3ed6d526b31d1a9c
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-09-02 17:28:04 +0300

    VFB-436: fixed jest tests

--- src/common/databaseFilters.test.ts
+++ src/common/databaseFilters.test.ts
@@ -294,7 +294,7 @@ describe("test for phone number filter", () => {
         // Assert
         expect(result).toBe(mockQueryWithOrAppended);
         expect(mockOr).toHaveBeenCalledWith(
-            "or(or(client_is_active.is.false, client_phone_number.ilike.%-%), or(client_is_active.is.false, client_additional_phone_numbers_text.ilike.%-%))"
+            "or(client_is_active.is.false, client_phone_number.ilike.%-%)"
         );
     });
 
@@ -306,9 +306,9 @@ describe("test for phone number filter", () => {
         expect(result).toBe(mockQueryWithOrAppended);
         expect(mockOr).toHaveBeenCalledWith(
             "or(and(client_is_active.is.true, client_phone_number.ilike.%substring1%), and(client_is_active.is.true, client_additional_phone_numbers_text.ilike.%substring1%))," +
-                "or(or(client_is_active.is.false, client_phone_number.ilike.%-%), or(client_is_active.is.false, client_additional_phone_numbers_text.ilike.%-%))," +
-                "or(and(client_is_active.is.true, client_phone_number.ilike.%sub string2%), and(client_is_active.is.true, client_additional_phone_numbers_text.ilike.%sub string2%))," +
-                "or(and(client_is_active.is.true, client_phone_number.ilike.%sub  string3%), and(client_is_active.is.true, client_additional_phone_numbers_text.ilike.%sub  string3%))"
+                "or(client_is_active.is.false, client_phone_number.ilike.%-%),or(and(client_is_active.is.true, client_phone_number.ilike.%sub string2%), " +
+                "and(client_is_active.is.true, client_additional_phone_numbers_text.ilike.%sub string2%)),or(and(client_is_active.is.true, client_phone_number.ilike.%sub  string3%), " +
+                "and(client_is_active.is.true, client_additional_phone_numbers_text.ilike.%sub  string3%))"
         );
     });
 });
