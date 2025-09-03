c737c5edb2418f86be321dac0b2af7c3802e01cd
Stefan Ghenescu <stefan.ghenescu@softwire.com>
2025-09-03 13:05:14 +0300

    VFB-427: update parcels_plus columns from previous migrations

--- supabase/migrations/20250903093837_add_sorted_postcodes_column_to_parcels_plus.sql
+++ supabase/migrations/20250903093837_add_sorted_postcodes_column_to_parcels_plus.sql
@@ -1,43 +1,48 @@
-create or replace view "public"."parcels_plus" as  SELECT parcels.primary_key AS parcel_id,
+create or replace view
+    "public"."parcels_plus" with (security_invoker = true) as
+select
+    parcels.primary_key as parcel_id,
     parcels.collection_datetime,
     parcels.packing_date,
     parcels.created_at,
-    packing_slots.name AS packing_slot_name,
-    packing_slots."order" AS packing_slot_order,
+    packing_slots.name as packing_slot_name,
+    packing_slots."order" as packing_slot_order,
     parcels.voucher_number,
-    collection_centres.name AS collection_centre_name,
-    collection_centres.acronym AS collection_centre_acronym,
+    collection_centres.name as collection_centre_name,
+    collection_centres.acronym as collection_centre_acronym,
     collection_centres.is_delivery,
-    clients.primary_key AS client_id,
-    clients.full_name AS client_full_name,
-    clients.address_postcode AS client_address_postcode,
-    clients.flagged_for_attention AS client_flagged_for_attention,
-    clients.signposting_call_required AS client_signposting_call_required,
-    clients.phone_number AS client_phone_number,
-    clients.is_active AS client_is_active,
+    clients.primary_key as client_id,
+    clients.full_name as client_full_name,
+    clients.address_postcode as client_address_postcode,
+    clients.flagged_for_attention as client_flagged_for_attention,
+    clients.signposting_call_required as client_signposting_call_required,
+    clients.phone_number as client_phone_number,
+    clients.is_active as client_is_active,
     family_count.family_count,
-    parcels_events.last_event_name AS last_status_event_name,
-    parcels_events.last_event_data AS last_status_event_data,
-    parcels_events.last_event_timestamp AS last_status_timestamp,
-    parcels_events.last_event_workflow_order AS last_status_workflow_order,
-    parcels_events.last_event_is_successfully_completed AS last_status_is_successfully_completed,
+    parcels_events.last_event_name as last_status_event_name,
+    parcels_events.last_event_data as last_status_event_data,
+    parcels_events.last_event_timestamp as last_status_timestamp,
+    parcels_events.last_event_workflow_order as last_status_workflow_order,
+    parcels_events.last_event_is_successfully_completed as last_status_is_successfully_completed,
     parcels_events.all_events,
-    clients.delivery_instructions AS client_delivery_instructions,
+    clients.delivery_instructions as client_delivery_instructions,
     parcels.list_type,
     parcels.referral_agency,
     parcels.referrer_name,
     parcels.referrer_email,
     parcels.referrer_phone,
-    clients.email AS client_email,
-    clients_plus.sorted_address_postcode AS sorted_client_address_postcode
-   FROM ((((((parcels
-     LEFT JOIN collection_centres ON ((parcels.collection_centre = collection_centres.primary_key)))
-     LEFT JOIN clients ON ((parcels.client_id = clients.primary_key)))
-     LEFT JOIN clients_plus ON ((clients_plus.client_id = clients.primary_key)))
-     LEFT JOIN packing_slots ON ((parcels.packing_slot = packing_slots.primary_key)))
-     LEFT JOIN family_count ON ((family_count.family_id = clients.family_id)))
-     LEFT JOIN parcels_events ON ((parcels_events.parcel_id = parcels.primary_key)))
-  ORDER BY parcels.packing_date DESC;
+    clients.email as client_email,
+    parcels.flagged_for_attention,
+    parcels.signposting_call_required
+    clients_plus.sorted_address_postcode as sorted_client_address_postcode
+from parcels
+     left join collection_centres on parcels.collection_centre = collection_centres.primary_key
+     left join clients on parcels.client_id = clients.primary_key
+     left join clients_plus on clients_plus.client_id = clients.primary_key
+     left join packing_slots on parcels.packing_slot = packing_slots.primary_key
+     left join family_count on family_count.family_id = clients.family_id
+     left join parcels_events on parcels_events.parcel_id = parcels.primary_key
+order by parcels.packing_date desc;
 
 
 
