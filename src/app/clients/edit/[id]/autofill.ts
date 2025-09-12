763ff11b88b83ca0b2c14f98016546cd64b29261
ZOO\AleMar <alexandra.mardale@softwire.com>
2025-09-12 14:33:55 +0300

    VFB-414: Postcode should remain null on autofill to indicate nfa clients

--- src/app/clients/edit/[id]/autofill.ts
+++ src/app/clients/edit/[id]/autofill.ts
@@ -22,7 +22,8 @@ const autofill = (
         addressLine2: noPostcode ? "" : clientData.address_2 ?? "",
         addressTown: noPostcode ? "" : clientData.address_town ?? "",
         addressCounty: noPostcode ? "" : clientData.address_county ?? "",
-        addressPostcode: clientData.address_postcode ?? "",
+        // This should not be set to the empty string because the null value is used to indicate an NFA client
+        addressPostcode: clientData.address_postcode,
         numberOfAdults: adults.length,
         adults: adults,
         numberOfChildren: children.length,
