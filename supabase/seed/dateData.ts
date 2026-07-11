0b025d8fadfc507f80d669034c3bd1917857d172
Lincoln Stone <lincoln.stone@softwire.com>
2026-07-11 11:13:55 +0100

    VFB-NONE: Update seed data date range

--- supabase/seed/dateData.ts
+++ supabase/seed/dateData.ts
@@ -1,9 +1,9 @@
 import seedrandom from "seedrandom";
 
-export const earliestParcelOrEventDate = new Date(2025, 5, 1); // 2025-06-01
-export const latestEventDate = new Date(2025, 11, 1); // 2025-12-01
-export const farFutureDate = new Date(2026, 5, 1); // 2026-06-01
-export const parcelCreationDateTime = new Date("2024-12-31T12:00:00");
+export const earliestParcelOrEventDate = new Date(2026, 5, 1); // 2026-06-01
+export const latestEventDate = new Date(2026, 11, 1); // 2026-12-01
+export const farFutureDate = new Date(2027, 5, 1); // 2027-06-01
+export const parcelCreationDateTime = new Date("2025-12-31T12:00:00");
 
 export function getPseudoRandomDateBetween(start: Date, end: Date, seed: string): Date {
     const randomNumberGenerator = seedrandom(seed);
